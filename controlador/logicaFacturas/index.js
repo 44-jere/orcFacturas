3c627e3768f448717587bb839379a3f2b3e5082f
Jeremy Bryan Ico Herrera <jeremyico78@gmail.com>
2025-09-01 23:59:18 -0600

    implementando adminMain

--- controlador/logicaFacturas/index.js
+++ controlador/logicaFacturas/index.js
@@ -9,6 +9,7 @@ const port = process.env.PORT ?? 8080;
 app.use(express.static(path.resolve(__dirname, "../../public")));
 app.use(express.static(path.resolve(__dirname, "../../public/mainfacturas")));
 app.use(express.static(path.resolve(__dirname, "../../public/login")));
+app.use(express.static(path.resolve(__dirname, "../../public/adminMain")));
 
 // sendFile will go here
 app.get("/", (req, res) =>
@@ -19,8 +20,8 @@ app.get("/login", (req, res) =>
   res.sendFile(path.join(__dirname, "../../public/login/login.html"))
 );
 
-app.get("/register", (req, res) =>
-  res.sendFile(path.join(__dirname, "../../public/register.html"))
+app.get("/admin", (req, res) =>
+  res.sendFile(path.join(__dirname, "../../public/adminMain/admin.html"))
 );
 
 app.listen(port);
